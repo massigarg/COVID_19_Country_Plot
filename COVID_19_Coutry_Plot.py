6257bb776a709ff6e5761f12979743985cea0925
Massimiliano <mas.gargano@gmail.com>
2020-04-23 11:29:11 +0200

    Choice loop added

--- COVID_19_Coutry_Plot.py
+++ COVID_19_Coutry_Plot.py
@@ -6,6 +6,10 @@ import pandas as pd
 import seaborn as sns
 import os
 
+'''
+All data from https://covid.ourworldindata.org/
+'''
+
 today = datetime.today().strftime("%d-%m-%Y")
 
 # #Total Deaths
@@ -15,6 +19,8 @@ if not os.path.isfile(f"./total_deaths_{today}.csv"):
 df_total_deaths = pd.read_csv(f"total_deaths_{today}.csv", parse_dates=["date"])
 df_total_deaths["date"] = pd.to_datetime(df_total_deaths["date"]).dt.strftime("%b-%d")
 df_total_deaths=df_total_deaths.fillna(method="bfill")[1:]
+df_total_deaths=df_total_deaths.iloc[-90:,:]
+df_total_deaths.reset_index(inplace=True)
 
 # #Total Confirmed Cases
 if not os.path.isfile(f"./total_cases_{today}.csv"):
@@ -22,27 +28,42 @@ if not os.path.isfile(f"./total_cases_{today}.csv"):
 
 df_total_cases = pd.read_csv(f"total_cases_{today}.csv", parse_dates=["date"])
 df_total_cases["date"] = pd.to_datetime(df_total_cases["date"]).dt.strftime("%b-%d")
-df_total_cases.set_index("date")
 df_total_cases = df_total_cases.fillna(method="bfill")[1:]
+df_total_cases=df_total_cases.iloc[-90:,:]
+df_total_cases.reset_index(inplace=True)
+
+
 
 # #New Confirmed Cases
 if not os.path.isfile(f"./new_cases_{today}.csv"):
     urllib.request.urlretrieve("https://covid.ourworldindata.org/data/ecdc/new_cases.csv", f"new_cases_{today}.csv")
 
 df_new_cases = pd.read_csv(f"new_cases_{today}.csv", parse_dates=["date"])
-df_new_cases["date"] = pd.to_datetime(df_new_cases["date"]).dt.strftime("%d-%m-%y")
-df_new_cases.set_index("date")
+df_new_cases["date"] = pd.to_datetime(df_new_cases["date"]).dt.strftime("%b-%d")
 df_new_cases = df_new_cases.fillna(method="bfill")[1:]
+df_new_cases=df_new_cases.iloc[-90:,:]
+df_new_cases.reset_index(inplace=True)
+
 
 def get_days():
+    '''
+    return: Object. Date column in str format
+    '''
     return df_total_deaths["date"]
 
+
 def get_countries():
+    '''
+    return List. List of all the countires in the DF
+    '''
     countries = list(df_total_deaths.columns.values)
     return countries
 
 
 def daily_death_increase():
+    '''
+    return List. List of the daily death increase
+    '''
     daily_increase = []
     for i in range(len(df_total_deaths[country])):
         if i == 0:
@@ -52,25 +73,32 @@ def daily_death_increase():
     return daily_increase
 
 
-
 def top_coutries():
-    top_countries = df_total_deaths.drop(labels=["date", "World"], axis=1)
-    top_countries.sort_values(by=len(top_countries.idxmax(1)), axis=1, ascending=False, inplace=True)
-    return list(top_countries.columns)
+    '''
+    return List. List of countries ordered by deaths number
+    '''
+    df_top_countries = df_total_deaths.drop(labels=["date", "World"], axis=1)
+    df_top_countries.sort_values(by=df_total_deaths.index[-1], axis=1, ascending=False, inplace=True)
+    return list(df_top_countries.columns)
 
 
 def top_deaths():
-    top_deaths = df_total_deaths.drop(labels=["date", "World"], axis=1)
-    top_deaths.sort_values(by=len(top_deaths.idxmax(1)), axis=1, ascending=False, inplace=True)
-    return list(top_deaths.iloc[-1])
+    '''
+    return List. List of values ordered by deaths number
+    '''
+    df_top_deaths = df_total_deaths.drop(labels=["date", "World"], axis=1)
+    df_top_deaths.sort_values(by=df_total_deaths.index[-1], axis=1, ascending=False, inplace=True)
+    return list(df_top_deaths.iloc[-1])
 
 
 def plot_figure():
     global country
-    country="World"
-    country=input("Choose a country: ")
-    if country not in get_countries():
-        return
+    while True:
+        country = input(f"Please choose a country or 'World': ")
+        if country in get_countries():
+            break
+        else:
+            print("Country not found")
 
     fig = plt.figure(figsize=(16, 9))
     fig.suptitle(f"Trends for {country} @{today}", fontsize=12, fontweight='bold', y=1)
@@ -97,7 +125,7 @@ def plot_figure():
     ax2.set_ylabel("Total Deats", color="red")
     ax2.tick_params(axis='y', labelcolor="red")
 
-    sns.lineplot(data=df_total_deaths[country], linewidth=2.5 , color="red",ax=ax2)
+    sns.lineplot(data=df_total_deaths[country].loc[-90:], linewidth=2.5 , color="red",ax=ax2)
 
     ax1.set_xlabel("Date")
 
